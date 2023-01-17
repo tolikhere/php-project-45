71cb8284c762303ebb5d9bafbdf0468c95e0f681
Anatoliy Pavlenko <fenixdurangoadrenalin@gmail.com>
2023-01-17 08:52:20 +0300

    Realize logic for a brain-prime game. Add command "brain-prime" into the Makefile to launch the brain-prime game

---
+++ src/Games/Prime.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace BrainGames\Games\Prime;
+
+// the function generate an expression for the game and returning an array with an expression and a correct answer
+function generateExpression($startRange, $endRange)
+{
+        $expression = mt_rand($startRange, $endRange);
+        $answer = isPrime($expression) ? 'yes' : 'no';
+
+        return [$expression, $answer];
+}
+
+function isPrime(int $num): bool
+{
+    if ($num < 2) {
+        return false;
+    }
+
+    $ceiling = $num / 2;
+    for ($i = 2; $i <= $ceiling; $i++) {
+        if ($num % $i === 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
